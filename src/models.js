ab1b2f13fd729dbee619fddb0aae0641af744167
Anthony Lukach <alukach@cadasta.org>
2017-08-10 13:21:06 -0600

    Handle expiration as minutes

--- src/models.js
+++ src/models.js
@@ -4,8 +4,9 @@ import uuidv4 from 'uuid/v4';
 import config from './config/config';
 
 function getExpirationDate() {
+  const lifetime = config.DATA_LIFETIME * 60
   let expirationDate = new Date();
-  expirationDate.setSeconds(expirationDate.getSeconds() + config.DATA_LIFETIME);
+  expirationDate.setSeconds(expirationDate.getSeconds() + lifetime);
   return Math.floor(expirationDate.getTime() / 1000)
 }
 
