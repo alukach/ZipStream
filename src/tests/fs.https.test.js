18839a9d46653bd1da97290d26e8dd2eeb10947d
Anthony Lukach <alukach@cadasta.org>
2017-08-17 13:48:42 -0400

    Add stub tests

---
+++ src/tests/fs.https.test.js
@@ -0,0 +1,13 @@
+import sinon from 'sinon';
+import chai, { expect } from 'chai';
+import { Readable } from 'stream';
+
+
+chai.config.includeStack = true;
+
+
+describe('## FS Backend: https', () => {
+  describe('# Whitelist sources', () => {
+
+  });
+});
