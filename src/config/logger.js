7788765a4b75ebb458a4f7c0bd647856ccd9890e
Anthony Lukach <alukach@cadasta.org>
2017-11-13 11:47:42 -0700

    fixup

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,13 +1,13 @@
 import winston from 'winston';
-import config from './config';
+// import config from './config';
 
 const logger = new (winston.Logger)({
   level: 'debug',
   transports: [
     new (winston.transports.Console)({
-      json: true, //config.NODE_ENV === 'production',
+      json: true, // config.NODE_ENV === 'production',
       colorize: true,
-      stringify: (obj) => JSON.stringify(obj),
+      stringify: obj => JSON.stringify(obj),
     })
   ]
 });
